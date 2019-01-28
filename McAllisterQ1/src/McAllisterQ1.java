3b18374b0224ffad39f82c0a1691d6a41ca056aa
Jackson Ludwig <ludwj61@mail.sunysuffolk.edu>
2019-01-28 18:41:12 -0500

    First commit

---
+++ McAllisterQ1/src/McAllisterQ1.java
@@ -0,0 +1,36 @@
+public class McAllisterQ1 
+{
+    public static void main(String[] args) 
+    {
+        int n; double total = 0;
+        String s; boolean prasEntered = false;
+        double[] price;
+        String[] name;
+        
+        //1- input and parse  the number of items, n
+        
+        //2-  declare 2 one dimensional parallel arrays: price and name
+        
+        //3- for(int i= 0; i<n; i++) // every item
+             {
+                // input and price and parse inputan item name
+                // add the new price to the total
+                // if statement to set peasEntered to true
+                //   that uses name[i].equalsIgnoreCase("peas");
+             }
+        //4- for(int i= n-1; i>=0; i--) // every item last to first    
+             {
+                // system console output price[i] and name[i]
+             }
+        // if statement to determine if peasEntered is true
+           {
+                
+                //output avg
+           }
+        // else output peas not entered
+           {
+        
+           }     
+    }
+    
+}
