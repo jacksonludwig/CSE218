0ae909bfe7f6d3bb7cdd984b50d44d908c5abeb2
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2019-01-29 09:18:52 -0500

    Q1

--- LudwigQ1/src/p1/Demo.java
+++ LudwigQ1/src/p1/Demo.java
@@ -1,19 +1,34 @@
-/*
- * Due Tuesday
- */
 package p1;
 
-/**
- *
- * @author Jackson
- */
+import javax.swing.JOptionPane;
+
 public class Demo {
+    
     public static void main(String[] args) {
-        int n; double total = 0;
-        String s; boolean prasEntered = false;
+        int size; double total = 0;
+        boolean peasEntered = false;
         double[] price;
         String[] name;
         
+        size = Integer.parseInt(JOptionPane.showInputDialog("Please Enter the Amount Of Items To Buy: "));
+        price = new double[size];
+        name = new String[size];
+ 
+        for(int i = 0; i < size; i++) {
+            name[i] = JOptionPane.showInputDialog("Please Enter Item's Name: ");
+            if(name[i].equalsIgnoreCase("Peas"))
+                peasEntered = true;
+            price[i] = Double.parseDouble(JOptionPane.showInputDialog("Please Enter Item's Price: "));
+            total += price[i];
+        }
         
+        for(int i = size - 1; i >= 0; i--) {
+            System.out.println(name[i] + " ($" + price[i] + ")");
+        }
+        
+        if(peasEntered) {
+            total /= size;
+            JOptionPane.showMessageDialog(null, "The average price is: " + String.format("%.2f", total));
+        }
     }
 }
