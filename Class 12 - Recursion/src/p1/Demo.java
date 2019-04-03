c7c5569dcfb0f7f4d62ac0bb82f1d1eaccba55f1
Jackson Ludwig <ludwj61@mail.sunysuffolk.edu>
2019-04-03 19:50:48 -0400

    recursion

--- Class 12 - Recursion/src/p1/Demo.java
+++ Class 12 - Recursion/src/p1/Demo.java
@@ -7,7 +7,9 @@ public class Demo {
     public static void main(String[] args) {
 //        System.out.println(factorial(11));
 //        System.out.println(exponent(2, 16));
-        System.out.println(fibonacci(40));
+//        System.out.println(fibonacci(3));
+//        System.out.println(sumOfFirstNInts(3));
+        hanoi(3, 1, 2, 3);
     }
 
     public static long factorial(int n) {
@@ -53,4 +55,54 @@ public class Demo {
         return gs;
     }
 
+    public static int sumOfFirstNInts(int n) {
+        count++;
+        System.out.println("Recursion invoked " + count + " times.");
+        // if it's the base case, return known result
+        if (n == 1) {
+            return 1;
+        }
+        // calculate the reduced problem(s) recursively (rp)
+        int rp = (sumOfFirstNInts(n - 1));
+        // use rp to calculate the solution
+        int gs = n + rp;
+        return gs;
+    }
+
+    public static void hanoi(int numRings, int from, int to, int extra) {
+//        count++;
+//        System.out.println("Recursion invoked " + count + " times.");
+        // if it's the base case, return known result
+        if (numRings == 1) {
+            System.out.println("move one ring from tower " + from + " to tower " + to);
+            return;
+        }
+        // calculate the reduced problem(s) recursively (rp)
+        hanoi(numRings - 1, from, extra, to); // rp1
+        System.out.println("move one ring from tower " + from + " to tower " + to);
+        hanoi(numRings - 1, extra, to, from); // rp2
+    }
+
+    // fibo dynamic 
+    public static int fiboDynamic(int n) {
+        count++;
+        System.out.println("Recursion invoked " + count + " times.");
+        // if it's the base case, return known result
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        // calculate the reduced problem(s) recursively (rp)
+        int rp1 = fibonacci(n - 1);
+        int rp2 = fibonacci(n - 2);
+        // dynamic part
+        if (values[n - 1] != 0) {
+            rp1 = values[n - 1];
+        } else {
+            rp1 = fib(n - 1);
+            values[n - 1] = rp1;
+        } // end dynamic
+        // use rp to calculate the solution
+        int gs = rp1 + rp2;
+        return gs;
+    }
 }
