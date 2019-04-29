943a0562f41751caeb470862821f16f73bf40035
Jackson Ludwig <ludwj61@mail.sunysuffolk.edu>
2019-04-29 12:18:03 -0400

    P7 probably done

--- LudwigP7/src/p1/HeapSortTemplate.java
+++ LudwigP7/src/p1/HeapSortTemplate.java
@@ -14,40 +14,48 @@ import javax.swing.JOptionPane;
         accurate (short circuit evaluation will make it wrong sometimes)
         can take out old output because we want a lot of values (10k+)
 
-*/
-
+ */
 public class HeapSortTemplate {
 
+    private static int numComparisons = 0;
+    private static int numSwaps = 0;
+
     public static void main(String[] args) {
-      //  int[] data = {7, 36, 19, 17, 3, 25, 1, 2, 4};
+        //  int[] data = {7, 36, 19, 17, 3, 25, 1, 2, 4};
 
         int size = Integer.parseInt(JOptionPane.showInputDialog("How Many Items to"
                 + " sort," + " n = ?"));
         int[] data = new int[size];
         randomValues(data);
-        System.out.println("Unsorted");
-        System.out.println(Arrays.toString(data));
-        System.out.println();
+//        System.out.println("Unsorted");
+//        System.out.println(Arrays.toString(data));
+//        System.out.println();
 
         heapSort(data);
         // reHeapDown(data.length, data, 0);
-
+        
+        double predictedSortEffort = 3 * data.length * (Math.log10(data.length) / Math.log10(2));
+        
+        JOptionPane.showMessageDialog(null, "Actual swaps = " + numSwaps + "; Predicted swaps = "
+                + String.format("%.2f", (2.0 / 3 * predictedSortEffort)) + "\nActual Sort effort = " 
+                + numComparisons + "; Predicted Sort effort = "
+                + String.format("%.2f", predictedSortEffort) + "; Min sort effort = " 
+                + String.format("%.2f", data.length * (Math.log10(data.length) / Math.log10(2))));
+        
         System.out.println("Sorted");
-        System.out.println(Arrays.toString(data));
+        // System.out.println(Arrays.toString(data));
     }
 
     public static void heapSort(int[] data) {
         // Step 1: 
-        
+
         // do nothing; remember rules
-        
         // Step 2:
-        
         // build initial heap using rhd
         for (int ip = data.length / 2 - 1; ip >= 0; ip--) { // ip = index of parent
             reHeapDown(data.length, data, ip); // doesnt use root as zero so it can traverse tree backwards
         }
-        
+
         // Step 3: 
         for (int i = 1; i <= data.length; i++) {
             swap(data, 0, data.length - i);
@@ -65,6 +73,7 @@ public class HeapSortTemplate {
         }
 
         if (rootIndex * 2 + 1 == size - 1) { // one child
+            numComparisons++;
             if (data[rootIndex] > data[rootIndex * 2 + 1]) { // root larger than its 1 child
                 return;
             } else {
@@ -74,9 +83,14 @@ public class HeapSortTemplate {
         }
 
         // root has two children
-        if (data[rootIndex] > data[rootIndex * 2 + 1] && data[rootIndex] > data[rootIndex * 2 + 2]) { // root is bigger than both
-            return;
+        numComparisons++;
+        if (data[rootIndex] > data[rootIndex * 2 + 1]) { // root is bigger than both
+            numComparisons++;
+            if (data[rootIndex] > data[rootIndex * 2 + 2]) {
+                return;
+            }
         } else { // root isn't bigger than both
+            numComparisons++;
             if (data[rootIndex * 2 + 1] > data[rootIndex * 2 + 2]) { // left child is larger than right
                 swap(data, rootIndex, rootIndex * 2 + 1);
                 reHeapDown(size, data, rootIndex * 2 + 1);
@@ -120,5 +134,6 @@ public class HeapSortTemplate {
         int temp = data[i1];
         data[i1] = data[i2];
         data[i2] = temp;
+        numSwaps++;
     }
 }
